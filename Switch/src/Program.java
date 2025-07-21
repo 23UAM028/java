780c4f1477e917bf72ec697c25ec847810d63a3e
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2025-07-21 20:27:51 +0530

    Add files via upload

---
+++ Switch/src/Program.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int button=sc.nextInt();
+    
+    switch(button) {
+    case 1:System.out.println("Hello");
+    break;
+    
+    case 2:System.out.println("namste");
+    break;
+
+    case 3:System.out.println("banjoer");
+    
+    default:System.out.println("Invalid button");
+
+
+    }
+    
+    
+	}
+
+}
