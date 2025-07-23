9d3a00b85c79367544bf9216285d90849a45e822
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2025-07-23 20:07:25 +0530

    Add files via upload

---
+++ ForLoop/src/Program.java
@@ -0,0 +1,34 @@
+////print the sum of first n Natural number
+//import java.util.Scanner;
+//
+//public class Program {
+//
+//	public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+//     int n=sc.nextInt();
+//     int sum=0;
+//     for(int i=0;i<=10;i++) {
+//    	 sum=sum+i;
+//     }
+//	 System.out.println(sum);
+//
+//	}
+//
+//}
+
+
+//print the table of a number input by the user
+
+import java.util.Scanner;
+public class Program {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    
+    for(int i=1;i<11;i++) {
+    	System.out.println(i*25);
+    }
+	}
+	
+}	
