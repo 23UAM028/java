5006ff4b62ae7dfe96e2e231abd90ff60f7fcbbe
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2025-07-27 12:41:11 +0530

    Add files via upload

---
+++ SampleQ2/src/PowerOfx.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PowerOfx {
+
+	public static void main(String[] args) {
+
+		System.out.print("Enter x:");
+		try (Scanner sc = new Scanner(System.in)) {
+			int x=sc.nextInt();
+			
+			System.out.print("Enter n:");
+			int n=sc.nextInt();
+			
+			int result=1;
+			for(int i=0;i<=n;i++) {
+				result=result * x;
+			}
+			System.out.println("x to the power n is:"+result);
+		}
+	}
+
+}
