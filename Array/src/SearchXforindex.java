8d63d1aebd526275b0e9e6d7bcc3a219923af000
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2025-07-29 21:14:51 +0530

    Add files via upload

---
+++ Array/src/SearchXforindex.java
@@ -0,0 +1,26 @@
+//take an array as input the user.search for a given number x and print the index at which it occurce
+
+import java.util.Scanner;
+
+public class SearchXforindex {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+	    int size=sc.nextInt();
+	    int number[]=new int[size];
+	    
+	    for(int i=0;i<size;i++) {
+	    	number[i]=sc.nextInt();
+	    }
+	    int x=sc.nextInt();
+	    
+	    for(int i=0;i<number.length;i++) {
+	    	if(number[i]==x) {
+	    		System.out.println("x found at index:"+i);
+	    	}
+	    }
+	
+	}
+
+}
