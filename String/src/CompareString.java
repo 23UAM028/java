360cedfdf2f60239a83e248385c8e01fd4018c98
Dikshant Dhanawade <dhanawadedikshant@gmail.com>
2025-07-30 21:11:29 +0530

    Add files via upload

---
+++ String/src/CompareString.java
@@ -0,0 +1,20 @@
+
+public class CompareString {
+
+	public static void main(String[] args) {
+
+		String name1="Dikshant";
+		String name2="Dhanawade";
+		//s1>s2:+ve value
+		//s1==s2:0
+		//s1<s2:negative
+		
+		if(name1.compareTo(name2)==0) {
+			System.out.println("String are equal");
+		}else {
+			System.out.println("Ststring are not equal");
+		}
+		
+	}
+
+}
---
+++ String/src/Concatenation.java
@@ -0,0 +1,17 @@
+//Concatenation=joining the name
+public class Concatenation {
+
+	public static void main(String[] args) {
+		String name="Dikshant";
+		String lastname="Dhanawade";
+		String fullname=name +" "+lastname;
+		System.out.println(fullname);
+		
+		//charAt=one-one character print
+		for(int i=0;i<fullname.length();i++) {
+			System.out.println(fullname.charAt(i));
+		}
+
+	}
+
+}
